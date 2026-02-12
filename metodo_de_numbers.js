a58305facad578fd9f6c96df8ff6f156fb144ef5
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-02-11 21:08:33 -0300

    metodo de numbers

---
+++ metodo_de_numbers.js
@@ -0,0 +1,38 @@
+
+let n = 10;
+
+let res = n.toString()+5;
+
+console.log(res);
+
+let n2 = 17.54647333;
+
+let res2 = n2.toFixed(3);
+
+console.log(res2);
+
+let n3 = '25';
+
+let resultado = n3 + 5;
+
+let res3 = parseInt(n3) + 5;
+
+console.log(resultado);
+console.log(res3);
+
+
+let n3float = '25.7';
+
+let resultadoFloat = n3float + 4;
+
+let n3FloatCorreto = parseFloat(n3float) + 4;
+
+console.log(resultadoFloat);
+
+console.log(n3FloatCorreto);
+
+/*
+    toFixed serve para informar quantas casas decimais deseja.
+    parseInt converte para inteiro
+    parseFloat converte para flutuante
+*/
